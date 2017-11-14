f76f04eb666d97fe666bdf9f8f69d92469465146
Michal Babej <michal.babej@tut.fi>
2017-11-14 14:09:23 +0100

    mem host flags / images: add extra check

--- test_conformance/mem_host_flags/checker_image_mem_host_no_access.hpp
+++ test_conformance/mem_host_flags/checker_image_mem_host_no_access.hpp
@@ -38,7 +38,7 @@ cl_int cImage_check_mem_host_no_access<T>:: verify_RW_Image()
 {
   this->Init_rect();
   
-  cl_event event;
+  cl_event event = nullptr;
   size_t img_orig[3] = {0, 0, 0};
   size_t img_region[3] = {0, 0, 0};
   img_region[0] = this->m_cl_Image_desc.image_width;
@@ -51,6 +51,7 @@ cl_int cImage_check_mem_host_no_access<T>:: verify_RW_Image()
                           &color,
                           img_orig, img_region,
                           0, NULL, &event);
+  test_error(err, "clEnqueueFillImage error");
   
   if (!this->m_blocking) {
     err = clWaitForEvents(1, &event);
